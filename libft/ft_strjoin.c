ce437ce04bdfb7c39202132bc49b8ef3d0663643
Johanna COURTOIS <jcourtoi@e1r8p8.clusters.42paris.fr>
2022-09-16 17:35:23 +0200

    norminette libft

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,15 +6,29 @@
 /*   By: nboratko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 19:18:02 by nboratko          #+#    #+#             */
-/*   Updated: 2022/09/08 16:51:33 by jcourtoi         ###   ########.fr       */
+/*   Updated: 2022/09/16 17:34:43 by jcourtoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static void	ft_strjoin_bis(char *s1, char *s2, char *s3, int len1)
+{
+	int	i;
+
+	i = -1;
+	while (s1[++i])
+		s3[i] = s1[i];
+	i = -1;
+	while (s2[++i])
+	{
+		s3[len1] = s2[i];
+		len1++;
+	}
+}
+
 char	*ft_strjoin(char *s1, char *s2)
 {
-	int		i;
 	int		len1;
 	int		dup;
 	char	*s3;
@@ -33,15 +47,7 @@ char	*ft_strjoin(char *s1, char *s2)
 	s3 = (char *)malloc(sizeof(char) * (len1 + ft_strlen(s2) + 1));
 	if (s3 == NULL)
 		return (NULL);
-	i = -1;
-	while (s1[++i])
-		s3[i] = s1[i];
-	i = -1;
-	while (s2[++i])
-	{
-		s3[len1] = s2[i];
-		len1++;
-	}
+	ft_strjoin_bis(s1, s2, s3, len1);
 	s3[len1] = '\0';
 	return (s3);
 }
