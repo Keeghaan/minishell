66ed91026dc7793b06402fa4cd568d56cea43ae1
Johanna COURTOIS <jcourtoi@e1r8p8.clusters.42paris.fr>
2022-09-16 10:30:55 +0200

    norminette builtins (unset)

--- builtins/unset.c
+++ builtins/unset.c
@@ -1,11 +1,32 @@
 #include "../inc/minishell.h"
 
+void	unset_envp_bis(t_envp *tmp, char *unset)
+{
+	t_envp	*tmp2;
+	t_envp	*tmp3;
+
+	tmp3 = tmp;
+	while (tmp3->next != NULL)
+	{
+		if (!ft_strncmp(tmp3->next->var, unset, ft_strlen(tmp3->next->var)))
+		{
+			tmp2 = tmp3->next;
+			tmp3->next = tmp3->next->next;
+			free(tmp2->var);
+			free(tmp2->values);
+			free(tmp2);
+			break ;
+		}
+		else
+			tmp3 = tmp3->next;
+	}
+}
+
 void	unset_envp(t_shell *shell, char *unset)
 {
 	t_envp	*tmp;
 	t_envp	*tmp2;
-	t_envp	*tmp3;
-	int	i;
+	int		i;
 
 	tmp = shell->envp;
 	i = 0;
@@ -18,22 +39,5 @@ void	unset_envp(t_shell *shell, char *unset)
 		free(tmp2);
 	}
 	else
-	{
-		tmp3 = tmp;
-		while (tmp3->next != NULL)
-		{
-			if (!ft_strncmp(tmp3->next->var, unset, ft_strlen(tmp3->next->var)))
-			{
-				tmp2 = tmp3->next;
-				tmp3->next = tmp3->next->next;
-				free(tmp2->var);
-				free(tmp2->values);
-				free(tmp2);
-				break ;
-			}
-			else
-				tmp3 = tmp3->next;
-		}
-	}
-
+		unset_envp_bis(tmp, unset);
 }
