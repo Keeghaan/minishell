a1edd6dfd89d7633020ec6470ce6e9e1435c44ac
Johanna COURTOIS <jcourtoi@e1r8p8.clusters.42paris.fr>
2022-09-16 13:24:01 +0200

    norminette pipex en cours

--- srcs/exec/pipex.c
+++ srcs/exec/pipex.c
@@ -68,15 +68,31 @@ static void	pipex_loop2(t_shell *child, int i, char **envp)
 		child_process(child, i, envp);
 }
 
+void	pipex_loop_bis(t_shell *child, t_cmd *tmp)
+{
+	if (ft_strncmp(tmp->infile, "/dev/stdin", ft_strlen(tmp->infile)))
+	{
+		dup2(child->std_in, 0);
+		child->infile = open(tmp->infile, O_RDONLY);
+		if (dup2(child->infile, STDIN_FILENO) == -1)
+			printf("%s: %s\n", SH, strerror(errno));
+		close(child->infile);
+	}
+	else
+	{
+		if (dup2(child->pipefd[0], STDIN_FILENO) == -1)
+			printf("%s: %s\n", SH, strerror(errno));
+		close(child->pipefd[0]);
+	}
+}
+
 static void	pipex_loop(t_shell *child, int i, char **envp)
 {
 	t_cmd	*tmp;
 	int		j;
 
 	tmp = child->cmds;
-	if (i == 0)
-		;
-	else
+	if (i != 0)
 	{
 		j = 0;
 		while (j < i)
@@ -89,22 +105,7 @@ static void	pipex_loop(t_shell *child, int i, char **envp)
 		}
 	}
 	if (i > 0)
-	{
-		if (ft_strncmp(tmp->infile, "/dev/stdin", ft_strlen(tmp->infile)))
-		{
-			dup2(child->std_in, 0);
-			child->infile = open(tmp->infile, O_RDONLY);
-			if (dup2(child->infile, STDIN_FILENO) == -1)
-				printf("%s: %s\n", SH, strerror(errno));
-			close(child->infile);
-		}
-		else
-		{
-			if (dup2(child->pipefd[0], STDIN_FILENO) == -1)
-				printf("%s: %s\n", SH, strerror(errno));
-			close(child->pipefd[0]);
-		}
-	}
+		pipex_loop_bis(child, tmp);
 	if (pipe(child->pipefd) == -1)
 		ft_printf("%s: %s\n", SH, strerror(errno));
 	if (i == 0)
