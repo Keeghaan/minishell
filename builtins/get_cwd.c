680e45ac62e0cda099b3221804de4abc8829f2b2
Johanna COURTOIS <jcourtoi@e1r6p4.clusters.42paris.fr>
2022-09-22 15:59:29 +0200

    minishell

--- builtins/get_cwd.c
+++ builtins/get_cwd.c
@@ -6,7 +6,7 @@
 /*   By: jcourtoi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 16:00:56 by jcourtoi          #+#    #+#             */
-/*   Updated: 2022/09/21 17:30:37 by jcourtoi         ###   ########.fr       */
+/*   Updated: 2022/09/22 15:59:12 by jcourtoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,10 @@ int	get_next_dir(t_shell *shell, char *dir)
 {
 	char	*tmp;
 
-	if (ft_strchr(dir, '/'))
-	{
-		if (access(dir, F_OK | R_OK) == 0)
-		{	
-			shell->next_dir = dir;
-			return (0);
-		}
+	if (access(dir, F_OK | R_OK) == 0)
+	{	
+		shell->next_dir = dir;
+		return (0);
 	}
 	if (*dir != '/')
 	{
